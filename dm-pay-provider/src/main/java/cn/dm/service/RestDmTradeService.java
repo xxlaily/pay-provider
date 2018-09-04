48a39e5806c3074df1509a59f6af4df58039502f
hai.dong <sdodh366@163.com>
2018-09-04 08:37:30 +0800

    修改一个类型转换的错误

--- dm-pay-provider/src/main/java/cn/dm/service/RestDmTradeService.java
+++ dm-pay-provider/src/main/java/cn/dm/service/RestDmTradeService.java
@@ -37,17 +37,15 @@ public class RestDmTradeService {
     @RequestMapping(value = "/insertTrade",method = RequestMethod.POST)
     public Integer insertTrade(@RequestBody DmItemMessageVo dmItemMessageVo) throws Exception {
         DmTrade qgTrade = new DmTrade();
-        qgTrade.setId(IdWorker.getId());
+        qgTrade.setId(IdWorker.getId().toString());
         qgTrade.setAmount(dmItemMessageVo.getAmount());
         qgTrade.setCreatedTime(Calendar.getInstance().getTime());
         qgTrade.setOrderNo(dmItemMessageVo.getOrderNo());
         qgTrade.setPayMethod(1);
         qgTrade.setTradeNo(dmItemMessageVo.getTradeNo());
-
-//        rabbitTemplate.convertAndSend("topicExchange", "key.toUpdateGoods", dmItemMessageVo);
-//        暂时只添加一个修改订单状态的消息
+//        发送消息修改订单状态
         rabbitTemplate.convertAndSend("topicExchange", "key.toUpdateOrder", dmItemMessageVo);
-        logger.info("调用provider>>>>>>>>>>>>>>>>>>");
+        logger.info("[insertTrade]" + "发送消息成功，消息的内容为："+dmItemMessageVo.toString());
         return dmTradeMapper.insertDmTrade(qgTrade);
     }
 }
